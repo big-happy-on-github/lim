714d6f5cc7d860d96ac8666d026f3a9059e47dd1
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-06-15 20:39:55 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,23 @@
-import header from '@components/Header'
-import footer from '@components/Footer'
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
-      <header>
-        <title>lims extravaganza</title>
+      <Head>
+        <title>lim extravaganza</title>
         <link rel="icon" href="/favicon.ico" />
-      </header>
+      </Head>
 
       <main>
-        <header title="lim's extravaganza" />
-        <p class="description">
+        <Header title="lim's extravaganza" />
+        <p className="description">
           A collection of miscellaneous <code>code</code>
         </p>
       </main>
 
-      <footer><footer />
+      <Footer />
     </div>
   )
 }
