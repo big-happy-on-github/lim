729dd8ab9a640b0de84f4e066745adbdeca00dc5
big-happy-on-github <107756857+big-happy-on-github@users.noreply.github.com>
2024-06-15 20:37:13 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import header from '@components/Header'
+import footer from '@components/Footer'
 
 export default function Home() {
   return (
